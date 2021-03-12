f3d8e4fc0deea7cf455a6d1e6061709866b4e068
teethoe <ardithhowaiman@gmail.com>
2021-03-13 00:37:08 +0800

    added trackbars

--- functions.py
+++ functions.py
@@ -2,14 +2,14 @@ import cv2
 import numpy as np
 
 
-def get_trap(img, r):
+def get_trap(img, z, ratios):
     height, width = img.shape[:2]
-    b = int(height * r * 2)
-    t = int(b * (3.5 / 5))
+    b = int(height * z * ratios[0])
+    t = int(b * ratios[1])
     xb = int((width - b) // 2)
     xt = int((width - t) // 2)
-    h = int((b * 7 ** 0.5) // 20)
-    y = int((height - h) // 2)
+    h = int(b * ratios[2])
+    y = ratios[3]
     tl = (xt, y)
     tr = (xt + t, y)
     bl = (xb, y + h)
@@ -18,13 +18,13 @@ def get_trap(img, r):
     return points
 
 
-def get_cnt(img, side, r):
+def get_cnt(img, side, z, rxy):
     height, width = img.shape[:2]
-    h = int(height * r)
+    h = int(height * z)
     if side % 2 == 0:
         w = h
     else:
-        w = h * 2
+        w = int(h * rxy)
     x = int((width - w) // 2)
     y = int((height - h) // 2)
     return x, y, w, h
@@ -39,24 +39,24 @@ def get_rect(cnt):
     return tl, tr, bl, br
 
 
-def draw_cnt(img, side, r):
+def draw_cnt(img, side, z, ratios):
     colour = (0, 255, 0)
     if side > 0:
         if side > 1:
-            x, y, w, h = get_cnt(img, side, r)
+            x, y, w, h = get_cnt(img, side, z, ratios[0])
             img = cv2.rectangle(img, (x, y), (x + w, y + h), colour, 2)
         else:
-            points = get_trap(img, r)
+            points = get_trap(img, z, ratios)
             for i in range(4):
                 img = cv2.line(img, points[i], points[(i + 2) % 4], colour, 2)
                 if i % 2 == 0:
-                    img = cv2.line(img, points[i], points[(i+1)%4], colour, 2)
+                    img = cv2.line(img, points[i], points[(i+1) % 4], colour, 2)
     return img
 
 
-def transform(img, r):
-    pts = np.float32(get_trap(img, r))
-    w, h = get_cnt(img, 1, r)[2:]
+def transform(img, z, ratios):
+    pts = np.float32(get_trap(img, z, ratios))
+    w, h = get_cnt(img, 1, z, ratios[0])[2:]
     dst = np.float32(get_rect([0, 0, w, h]))
     M = cv2.getPerspectiveTransform(pts, dst)
     warped = cv2.warpPerspective(img, M, (w, h))
@@ -66,6 +66,6 @@ def transform(img, r):
 def stitch(images):
     bottom = cv2.hconcat(images[1:])
     top = np.zeros(bottom.shape, np.uint8)
-    top[0:top.shape[0], int(top.shape[1]/6):int(top.shape[1]/2)] = images[0]
+    top[0:top.shape[0], images[1].shape[1]:(images[1].shape[1]+images[2].shape[1])] = images[0]
     mosaic = cv2.vconcat([top, bottom])
     return mosaic
--- main.py
+++ main.py
@@ -1,19 +1,38 @@
-import cv2
 from functions import *
 
-RATIO = 0.5
-
+ZOOM = 0.5
+rxy = 1.00
+rtb = 1.00
+rhb = 0.50
+y1 = 100
+ratios = [rxy, rtb, rhb, y1]
 images = ['' for i in range(5)]
 side = -1
-#url = ''
+
+
+def nothing(x):
+    pass
+
+
+#url = 'https://192.168.0.199:8080'
 #cap = cv2.VideoCapture(url+'/video')
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(4)
+
+cv2.namedWindow('ratios')
+cv2.createTrackbar('y:x', 'ratios', 0, 200, nothing)
+cv2.createTrackbar('t:b', 'ratios', 0, 50, nothing)
+cv2.createTrackbar('h:b', 'ratios', 0, 40, nothing)
+cv2.createTrackbar('y1', 'ratios', 0, 400, nothing)
 
 while True:
     ret, frame = cap.read(1)
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
-    x, y, w, h = get_cnt(frame, side, RATIO)
-    draw_cnt(frame, side, RATIO)
+    ratios[0] = (cv2.getTrackbarPos('y:x', 'ratios') + 100) / 100
+    ratios[1] = (cv2.getTrackbarPos('t:b', 'ratios') + 50) / 100
+    ratios[2] = (cv2.getTrackbarPos('h:b', 'ratios') + 5) / 100
+    ratios[3] = cv2.getTrackbarPos('y1', 'ratios') + 100
+    x, y, w, h = get_cnt(frame, side, ZOOM, ratios[0])
+    draw_cnt(frame, side, ZOOM, ratios)
     cv2.imshow('cam', frame)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
@@ -31,7 +50,7 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-images[0] = transform(images[0], RATIO)
+images[0] = transform(images[0], ZOOM, ratios)
 mosaic = stitch(images)
 cv2.imshow('mosaic', mosaic)
 
